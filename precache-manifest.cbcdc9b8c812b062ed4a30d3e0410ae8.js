73a781fefd29ca72ab7255e8203f87746bbd6659
mgrinko <glukym@gmail.com>
2019-03-04 16:14:56 +0200

    Updates

---
+++ precache-manifest.cbcdc9b8c812b062ed4a30d3e0410ae8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1e3d59b6263ca84f5484",
+    "url": "/fs_on_dec18-todo-app/static/css/main.0408fb36.chunk.css"
+  },
+  {
+    "revision": "1e3d59b6263ca84f5484",
+    "url": "/fs_on_dec18-todo-app/static/js/main.1e3d59b6.chunk.js"
+  },
+  {
+    "revision": "642acc9deef33f4cc336",
+    "url": "/fs_on_dec18-todo-app/static/js/runtime~main.642acc9d.js"
+  },
+  {
+    "revision": "c88c70e5f4ff8bea6fac",
+    "url": "/fs_on_dec18-todo-app/static/js/2.c88c70e5.chunk.js"
+  },
+  {
+    "revision": "1ad1e259426381869acc8c46c3d661f7",
+    "url": "/fs_on_dec18-todo-app/index.html"
+  }
+];
